b42a408dc8175b87c2cdeafc1e8c7a410921e6a2
Simon Yin <simon.yin1@unsw.edu.au>
2020-03-06 11:43:12 +1100

    A small change to data value to make the example more meaningful

--- docker/src/010-subroutine.f90
+++ docker/src/010-subroutine.f90
@@ -4,7 +4,7 @@ program pg10  ! A subroutine contained in a parent program
     integer :: q1, i141, i42
     i141 = 141
     i42  = 42
-    q1   = 1
+    q1   = 11
     call insub1(q1)
     print *, "q1: ", q1
     print *, "i42: ", i42
