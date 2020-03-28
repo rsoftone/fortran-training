7393cd29b70fe39e17c18403ff4212c4426b728a
Simon Yin <54009146+rsoftone@users.noreply.github.com>
2020-03-28 16:44:26 +1100

    Update helper.sh

--- docker/src/helper.sh
+++ docker/src/helper.sh
@@ -2,4 +2,5 @@
 SRCFILE="${1}.f90"
 EXENAME="${1}"
 gfortran -o2 -Wall "${SRCFILE}" -o "${EXENAME}" -L/usr/lib -llapack -lblas && ./"${EXENAME}"
-
+# Fortran 77
+# gfortran -std=legacy -o2 -Wall "${SRCFILE}" -o "${EXENAME}" -L/usr/lib -llapack -lblas && ./"${EXENAME}"
